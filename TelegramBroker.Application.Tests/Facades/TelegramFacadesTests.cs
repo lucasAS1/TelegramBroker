a794e53d6b11680fe9f0a051df4800a5e1c3e4c2
lucasAS1 <lucas.a.silva1@outlook.com>
2023-03-24 19:50:13 -0300

    resolvendo teste unitario

--- TelegramBroker.Application.Tests/Facades/TelegramFacadesTests.cs
+++ TelegramBroker.Application.Tests/Facades/TelegramFacadesTests.cs
@@ -1,4 +1,7 @@
-﻿using Moq;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using ChatbotProject.Common.Domain.Models.Requests;
+using Moq;
 using RabbitMQ.Client.Core.DependencyInjection.Services.Interfaces;
 using TelegramBroker.Domain.Facades.Telegram;
 using TelegramBroker.Domain.Models.Responses;
@@ -9,18 +12,23 @@ namespace TelegramBroker.Application.Tests.Facades;
 public class TelegramFacadesTests
 {
     private readonly Mock<IProducingService> _queueService;
+    private readonly IFixture _fixture;
 
     public TelegramFacadesTests()
     {
         _queueService = new Mock<IProducingService>();
+        _fixture = new Fixture();
+
+        _fixture.Customize(new AutoMoqCustomization() { ConfigureMembers = true });
     }
 
     [Fact]
     public void ShouldSendMessageCorrectly()
     {
         var aut = new TelegramFacade(_queueService.Object);
+        var webhookResponse = _fixture.Create<WebhookResponse>();
         
-        aut.SendMessage(new WebhookResponse(){});
-        _queueService.Verify(x => x.Send(It.IsAny<WebhookResponse>(), It.IsAny<string>(),It.IsAny<string>()), Times.Once);
+        aut.SendMessage(webhookResponse);
+        _queueService.Verify(x => x.Send(It.IsAny<MessageRequest>(), It.IsAny<string>(),It.IsAny<string>()), Times.Once);
     }
 }
